c574ac1a29dea6e69bdc05050f5dec6d43baf91e
Gagah Kharismanuary <gmanuary@gmail.com>
2019-11-12 17:36:20 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./assets/styles/main.scss";
 
-function App() {
+const App = () => {
+  const [imageSearch, setImageSearch] = useState({});
+  const [inputSearch, setInputSearch] = useState("");
+
+  useEffect(() => {
+    const API_URL = `https://api.giphy.com/v1/gifs/search?api_key=oM1Eg1dakdnN1TCijwtKrNA4PJNQLTuW&q=${inputSearch}&limit=8&offset=0&rating=G&lang=en`;
+    const imageData = async () => {
+      const result = await axios(API_URL);
+      setImageSearch(result.data.data);
+    };
+    imageData();
+  }, [inputSearch]);
+
+  if (imageSearch.length === undefined) return null;
+
+  console.log(imageSearch);
+
+  const handleChange = event => {
+    setInputSearch(event.target.value);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span>Galleryeasy</span>
       </header>
+      <section>
+        <div className="form-field">
+          <div className="form-field__control">
+            <input
+              id="exampleField"
+              type="text"
+              className="form-field__input"
+              placeholder="Search"
+              onChange={e => handleChange(e)}
+            />
+          </div>
+        </div>
+        <div className="col-12 d-flex row text-center">
+          {imageSearch.map(item => (
+            <div className="col-4 col-sm-3" key={item.id}>
+              <img
+                className="img-default"
+                src={item.images.preview_webp.url}
+                alt=""
+              />
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
